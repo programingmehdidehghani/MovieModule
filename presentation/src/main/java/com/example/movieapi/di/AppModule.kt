1a650cc03e73a48697049da90ff9801952dfe37f
mehdi <dehghani_m@rocketmail.com>
2023-05-02 17:08:20 +0430

    continue make presention

---
+++ presentation/src/main/java/com/example/movieapi/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.example.movieapi.di
+
+import com.example.data.repository.MoviesRepositoryImpl
+import com.example.data.services.MoviesServices
+import com.example.domain.repositories.MoviesRepository
+import org.koin.dsl.module
+
+
+val repositoryModules = module {
+    single {
+
+    }
+}
+
+/*val interactorModules = module {
+    factory (name =  GET_MOVIES_INTERACTOR){
+        GetMoviesInteractor(repositoryModules = get())
+    }
+}*/
+
+/*val networkModules = module {
+    single(name = RETROFIT_INSTANCE) {
+        createNetworkClient(BASE_URL)
+    }
+}*/
+
+private fun provideMoviesRepository(moviesServices: MoviesServices): MoviesRepository =
+    MoviesRepositoryImpl(moviesServices = moviesServices)
+
+const val IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+private const val BASE_URL = "https://api.themoviedb.org/"
+private const val RETROFIT_INSTANCE = "Retrofit"
+private const val API = "Api"
+private const val GET_MOVIES_INTERACTOR = "getMoviesInteractor"
