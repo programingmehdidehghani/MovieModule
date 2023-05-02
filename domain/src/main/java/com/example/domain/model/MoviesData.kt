4b3e92893f24387155b0cc23a4d21a318f5a8cb3
mehdi <dehghani_m@rocketmail.com>
2023-05-02 15:39:03 +0430

    make model movie items

---
+++ domain/src/main/java/com/example/domain/model/MoviesData.kt
@@ -0,0 +1,8 @@
+package com.example.domain.model
+
+import com.google.gson.annotations.SerializedName
+
+data class MoviesData(
+    @SerializedName("page") var page: Int?,
+    @SerializedName("results") var results: List<MoviesItemData>
+)
