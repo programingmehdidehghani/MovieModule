bb7a3897611982f89615a9ba106e47038654fe12
mehdi <dehghani_m@rocketmail.com>
2023-05-03 16:03:04 +0430

    make activites

--- presentation/src/main/java/com/example/movieapi/movies/MoviesActivity.kt
+++ presentation/src/main/java/com/example/movieapi/movies/MoviesActivity.kt
@@ -2,11 +2,15 @@ package com.example.movieapi.movies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.domain.model.MoviesItemData
 import com.example.movieapi.R
 import com.example.movieapi.databinding.ActivityDetailMoviesBinding
 import com.example.movieapi.databinding.ActivityMoviesBinding
+import com.example.movieapi.extension.observeResponse
 
 class MoviesActivity : AppCompatActivity() {
 
@@ -18,11 +22,12 @@ class MoviesActivity : AppCompatActivity() {
     private val viewBinding by lazy {
         ActivityMoviesBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
         listAdapter = MoviesListAdapter()
-        layoutManager = GridLayoutManager(this,2)
+        layoutManager = GridLayoutManager(this, 2)
         viewBinding.recyclerViewMovies.layoutManager = layoutManager
         viewBinding.recyclerViewMovies.adapter = listAdapter
         moviesViewModel.getMovies(loadingPage)
@@ -31,6 +36,33 @@ class MoviesActivity : AppCompatActivity() {
             loadingPage++
             moviesViewModel.getMovies(loadingPage)
         }
+
+        moviesViewModel.moviesLiveData.observeResponse(
+            owner = this,
+            loading = { loading ->
+                 Toast.makeText(this, loading , Toast.LENGTH_SHORT).show()
+            },
+            fail = { error ->
+                 Toast.makeText(this,error,Toast.LENGTH_SHORT).show()
+            },
+            success = { moviesData ->
+                  moviesData?.let {
+                      listAdapter.updateList(moviesData.results as MutableList<MoviesItemData>)
+                  }
+            }
+        )
+        viewBinding.searchViewMovies.setOnQueryTextListener( object : SearchView.OnQueryTextListener,
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText.isNullOrEmpty() || newText.length > 2)
+                    listAdapter.filter.filter(newText)
+                return false
+            }
+        })
 
     }
 }
