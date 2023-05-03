2ab763f731ddbbf497721c52aa1610f499bdf5cc
mehdi <dehghani_m@rocketmail.com>
2023-05-03 11:42:31 +0430

    add rx retrofit

--- presentation/src/main/java/com/example/movieapi/di/Network.kt
+++ presentation/src/main/java/com/example/movieapi/di/Network.kt
@@ -1,13 +1,40 @@
 package com.example.movieapi.di
 
+import com.example.movieapi.BuildConfig
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 
+
+class BasicAuthInterceptor(): Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val newUrl =
+            request.url.newBuilder().addQueryParameter("api_key",BuildConfig.API_KEY).build()
+        val newRequest = request.newBuilder().url(newUrl).build()
+        return chain.proceed(newRequest)
+    }
+
+}
+
+private fun httpClient(): OkHttpClient {
+    val httpLoggingInterceptor = HttpLoggingInterceptor(HttpLoggingInterceptor.Logger.DEFAULT)
+    val clientBuilder = OkHttpClient.Builder()
+    if (BuildConfig.DEBUG){
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        clientBuilder.addInterceptor(httpLoggingInterceptor)
+    }
+    clientBuilder.addInterceptor(Basic)
+}
 private fun retrofitClient(baseUrl: String, httpClient: OkHttpClient): Retrofit =
     Retrofit.Builder()
         .baseUrl(baseUrl)
         .client(httpClient)
         .addConverterFactory(GsonConverterFactory.create())
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.c)
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .build()
